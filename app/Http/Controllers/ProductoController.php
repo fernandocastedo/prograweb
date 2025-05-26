e3aba63a14bbe8cd5c8f9667cbc01335a963b83c
fernandocastedo <cmpfernando.ouac@gmail.com>
2025-05-25 23:34:57 -0400

    Proyecto con laravel finalizado

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -11,15 +11,33 @@ class ProductoController extends Controller
     {
         $categoria = $request->input('categoria');
 
-        $productos = Producto::when($categoria, function ($query, $categoria) {
-            return $query->where('categoria', $categoria);
-        })->get();
+        $productos = Producto::all()->filter(function ($producto) use ($categoria) {
+            $imagenBase = pathinfo($producto->imagen, PATHINFO_FILENAME);
+            $mapaCategorias = [
+                'producto1' => 'Iluminación',
+                'producto2' => 'Adhesivos',
+                'producto3' => 'Adhesivos',
+                'producto4' => 'Seguridad',
+                'producto5' => 'Protección del hogar',
+                'producto6' => 'Protección del hogar',
+                'producto7' => 'Protección del hogar',
+                'producto8' => 'Protección del hogar',
+                'producto9' => 'Herramientas',
+                'producto10' => 'Seguridad',
+            ];
+
+            $categoriaProducto = $mapaCategorias[$imagenBase] ?? null;
+
+            return !$categoria || $categoriaProducto === $categoria;
+        });
+
 
         $categorias = collect([
-            'Herramientas',
-            'Seguridad',
+            'Iluminación',
             'Adhesivos',
-            'Hogar',
+            'Seguridad',
+            'Protección del hogar',
+            'Herramientas',
         ]);
 
         return view('productos.index', compact('productos', 'categorias', 'categoria'));
